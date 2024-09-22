5ea5ddc46f9915acdaf18839bbd43a1256c10c5d
Liu Baihao <liubaihaohello@outlook.com>
2024-09-22 21:17:46 +0800

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
 plugins {
-    java
-    `maven-publish`
-    signing
     kotlin("jvm") version "2.0.20"
 }
-
-val repoUri = "https://github.com/sharedwonder/hyplookup"
-val pkgUri = "https://maven.pkg.github.com/sharedwonder/maven-repository"
 
 group = "net.sharedwonder"
 version = property("version").toString()
@@ -14,7 +8,6 @@ version = property("version").toString()
 repositories {
     mavenLocal()
     mavenCentral()
-    maven(pkgUri)
 }
 
 dependencies {
@@ -36,56 +29,6 @@ java {
 
 kotlin {
     compilerOptions.freeCompilerArgs.add("-Xjvm-default=all")
-}
-
-publishing {
-    repositories {
-        mavenLocal()
-        maven(pkgUri) {
-            name = "GitHubPackages"
-            credentials {
-                username = findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
-                password = findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-
-    publications {
-        register<MavenPublication>("maven") {
-            from(components["java"])
-
-            pom {
-                name = project.name
-                description = project.description
-                url = repoUri
-
-                licenses {
-                    license {
-                        name = "The Apache License, Version 2.0"
-                        url = "https://www.apache.org/licenses/LICENSE-2.0"
-                    }
-                }
-
-                developers {
-                    developer {
-                        id = "sharedwonder"
-                        name = "Liu Baihao"
-                        email = "liubaihaohello@outlook.com"
-                    }
-                }
-
-                scm {
-                    connection = "scm:git:$repoUri.git"
-                    developerConnection = "scm:git:$repoUri.git"
-                    url = repoUri
-                }
-            }
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications["maven"])
 }
 
 tasks.withType<JavaCompile> {
